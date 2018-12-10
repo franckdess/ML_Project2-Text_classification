531fa137789a48836f5344f3b826dfcc4da93d55
Franck Dessimoz <franck.dessimoz@epfl.ch>
2018-12-10 20:04:17 +0100

    Updated functions.py and 2nd version of project -> 85.5%

--- functions.py
+++ functions.py
@@ -13,6 +13,24 @@ def tweet_to_vect(tweet, voc_cut, all_vects):
             vect += all_vects[i[0][0]]
     return vect/len(word_array)
 
+def weighted_tweet_to_vect(tweet, voc_cut, all_vects, score_pos, score_neg, dict_):
+    """ Map a tweet to a vector using the vector of each
+        word that is present in the tweet and multiply it
+        by its TF-IDF value. """
+    word_array = re.findall(r'\w+', tweet)
+    vect = np.zeros(20)
+    for word in word_array:
+        i = np.argwhere(voc_cut == word)
+        if(word in dict_):
+            id_in_dict = dict_[word]
+            score = np.maximum(score_pos[0, id_in_dict], score_neg[0, id_in_dict])
+            if(i.shape[0] != 0): 
+                vect += score * all_vects[i[0][0]]
+        else:
+            if(i.shape[0] != 0): 
+                vect += all_vects[i[0][0]]
+    return vect/len(word_array)
+
 def clean_data(array):
     """ Clean the data by deleting the id
         placed in the front of the tweet."""
@@ -32,9 +50,10 @@ def zero_to_neg(array):
 def build_submission(y_pred, id_submission):
     """ Build submission and save it into the
         folder 'prediction'."""
-    ret = np.ones((len(y_pred), 2))
-    for i in range(len(y_pred)):
-        ret[i] = np.array([i+1, y_pred[i]])
+    y_pred_ = zero_to_neg(y_pred)
+    ret = np.ones((len(y_pred_), 2))
+    for i in range(len(y_pred_)):
+        ret[i] = np.array([i+1, y_pred_[i]])
     ret = ret.astype(int)
     sub = pd.DataFrame(data = ret)
     sub.columns = ['Id', 'Prediction']
